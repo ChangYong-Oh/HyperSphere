818d486ccfacf9de26dc51e2f4be81fb9f4b11df
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-08-30 20:10:35 +0200

    first push

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "HyperSphere",
+    author = "ChangYong-Ohs",
+    version = 0,
+    dependency_links = (),
+    install_requires = ['numpy'],
+    scripts = [],
+    packages=find_packages(),
+    )
