0716a36227ee3a4492500ecb0dd002ea8cd3c778
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-09-27 20:24:31 +0200

    up-to-date

--- batch_run.sh
+++ batch_run.sh
@@ -2,10 +2,10 @@
 
 python HyperSphere/BO/sphere_bayesian_optimization.py rosenbrock 5 100
 python HyperSphere/BO/cube_bayesian_optimization.py rosenbrock 5 100
-python HyperSphere/BO/sphere_bayesian_optimization.py rosenbrock 10 100
-python HyperSphere/BO/cube_bayesian_optimization.py rosenbrock 10 100
+python HyperSphere/BO/sphere_bayesian_optimization.py rosenbrock 10 200
+python HyperSphere/BO/cube_bayesian_optimization.py rosenbrock 10 200
 
 python HyperSphere/BO/sphere_bayesian_optimization.py levy 5 100
 python HyperSphere/BO/cube_bayesian_optimization.py levy 5 100
-python HyperSphere/BO/sphere_bayesian_optimization.py levy 10 100
-python HyperSphere/BO/cube_bayesian_optimization.py levy 10 100
+python HyperSphere/BO/sphere_bayesian_optimization.py levy 10 200
+python HyperSphere/BO/cube_bayesian_optimization.py levy 10 200
