58682c11ca1e728a28cb85bb03e60d067b9b058d
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-10-14 01:29:40 +0200

    up to date

--- HyperSphere/GP/kernels/modules/stationary.py
+++ HyperSphere/GP/kernels/modules/stationary.py
@@ -12,8 +12,8 @@ class Stationary(Kernel):
 	def __init__(self, ndim, input_map=None, max_ls=None):
 		super(Stationary, self).__init__(input_map)
 		if max_ls is None:
-			self.max_ls = 2.0 * self.ndim ** 0.5
-		self.max_ls = max_ls
+			self.max_log_ls = np.log(2.0 * self.ndim ** 0.5)
+		self.max_log_ls = np.log(max_ls)
 		self.ndim = ndim
 		self.log_ls = Parameter(torch.FloatTensor(ndim))
 
@@ -23,15 +23,15 @@ class Stationary(Kernel):
 
 	def init_parameters(self, amp):
 		super(Stationary, self).init_parameters(amp)
-		self.log_ls.data.fill_(np.log(self.max_ls / 3.0))
+		self.log_ls.data.fill_(self.max_log_ls - np.log(2.0))
 
 	def out_of_bounds(self, vec=None):
 		if vec is None:
 			if not super(Stationary, self).out_of_bounds():
-				return (self.log_ls.data > math.log(2.0 * self.ndim ** 0.5)).any() or (self.log_ls.data < log_lower_bnd).any()
+				return (self.log_ls.data > self.max_log_ls).any() or (self.log_ls.data < log_lower_bnd).any()
 		else:
 			if not super(Stationary, self).out_of_bounds(vec[:super(Stationary, self).n_params()]):
-				return (vec[1:] > math.log(2.0 * self.ndim ** 0.5)).any() or (vec < log_lower_bnd).any()
+				return (vec[1:] > self.max_log_ls).any() or (vec < log_lower_bnd).any()
 		return True
 
 	def n_params(self):
@@ -53,7 +53,7 @@ class Stationary(Kernel):
 
 	def prior(self, vec):
 		n_param_super = super(Stationary, self).n_params()
-		return super(Stationary, self).prior(vec[:n_param_super]) + smp.uniform(np.exp(vec[n_param_super:]), lower=0.0, upper=self.max_ls)
+		return super(Stationary, self).prior(vec[:n_param_super]) + smp.uniform(np.exp(vec[n_param_super:]), lower=0.0, upper=np.exp(self.max_log_ls))
 
 	def __repr__(self):
 		return self.__class__.__name__ + ' (' + 'dim=' + str(self.ndim) + ')'
