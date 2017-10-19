2639639217ece2f3685d7e611ca550d1797a8782
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-10-19 13:09:52 +0200

    conda venv configuration

--- setup_conda.sh
+++ setup_conda.sh
@@ -3,4 +3,6 @@ mv HyperSphere HyperSphere_TBR
 cp -a HyperSphere_TBR/. ./
 rm -rf HyperSphere_TBR
 source activate HyperSphere
-conda install --yes --file requirements.txt
+conda install --yes pytorch torchvision -c soumith -n HyperSphere
+conda install --yes --file requirements.txt -n HyperSphere
+
