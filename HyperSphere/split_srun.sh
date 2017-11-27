89dbbbb43930dc5ddb920e8c6abdb3e0c2beab13
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-11-27 03:23:07 +0100

    split srun script

--- HyperSphere/split_srun.sh
+++ HyperSphere/split_srun.sh
@@ -32,7 +32,7 @@ esac
 
 for s in {1..5}
 do
-    dir_name=$($run_command $exp_config -d 100 -f levy | tail -1)
+    dir_name=$("$run_command $exp_config -d $2 -f levy | tail -1")
     for (( i=1; i<=$n_eval; i++ ))
     do
         $run_command -p $dir_name
