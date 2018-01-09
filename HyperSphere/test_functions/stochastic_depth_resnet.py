d9f999bbac26f9dbf218618959464321f9977e34
ChangYong-Oh <changyong.oh0224@gmail.com>
2018-01-09 21:15:30 +0100

    resnet print COMMAND

--- HyperSphere/test_functions/stochastic_depth_resnet.py
+++ HyperSphere/test_functions/stochastic_depth_resnet.py
@@ -43,6 +43,8 @@ def _stochastic_depth_resnet(probability_tensor, data_type):
 	cmd_str += ' --arch resnet --depth 110 --death-mode chosen --death-rate-filename ' + probability_filename
 	cmd_str += ' --save ' + save_dir
 	cmd_str += ' --batch-size 256 --epoch 500'
+	print(('=' * 20) + 'COMMAND' + ('=' * 20))
+	print(cmd_str)
 	process = subprocess.Popen(cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 	lastline = ''
 	while True:
