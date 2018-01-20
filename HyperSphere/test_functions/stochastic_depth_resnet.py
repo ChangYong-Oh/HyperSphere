1e127a29fea33b680c48fbc901faaca03acd8b1f
ChangYong-Oh <changyong.oh0224@gmail.com>
2018-01-20 01:14:05 +0100

    transformation change

--- HyperSphere/test_functions/stochastic_depth_resnet.py
+++ HyperSphere/test_functions/stochastic_depth_resnet.py
@@ -30,8 +30,8 @@ def transform_with_center(x, center_probability=0.5):
 	if isinstance(center_probability, (float, int)):
 		center_probability = x.clone() * 0 + center_probability
 	assert x.numel() == center_probability.numel()
-	assert torch.sum(center_probability > 1.0) == 0
-	assert torch.sum(center_probability < 0.0) == 0
+	assert torch.sum(center_probability > 1.0)[0] == 0
+	assert torch.sum(center_probability < 0.0)[0] == 0
 
 	shift = []
 	for i in range(center_probability.numel()):
