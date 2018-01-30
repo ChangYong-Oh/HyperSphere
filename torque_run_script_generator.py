a24ad2d73521025f22854376e3ae1008735f14eb
ChangYong-Oh <changyong.oh0224@gmail.com>
2018-01-30 16:07:12 +0100

    torque script generator

--- torque_run_script_generator.py
+++ torque_run_script_generator.py
@@ -13,20 +13,20 @@ HEAD += "module load python\n"
 HEAD += "\n"
 
 BODY_INITIATING = ""
-BODY_INITIATING += "INIT_COMMAND_CUBE_ROOT = \"python HyperSphere/BO/run_BO.py --parallel -g cube\"\n"
-BODY_INITIATING += "INIT_COMMAND_SPHERE_ROOT = \"python HyperSphere/BO/run_BO.py --parallel -g sphere --origin\"\n"
+BODY_INITIATING += "INIT_COMMAND_CUBE_ROOT=\"python HyperSphere/BO/run_BO.py --parallel -g cube\"\n"
+BODY_INITIATING += "INIT_COMMAND_SPHERE_ROOT=\"python HyperSphere/BO/run_BO.py --parallel -g sphere --origin\"\n"
 BODY_INITIATING += "case \"$ALGORITHM\" in\n"
-BODY_INITIATING += "  cube) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_CUBE_ROOT\"\n"
+BODY_INITIATING += "  cube) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_CUBE_ROOT\"\n"
 BODY_INITIATING += "    ;;\n"
-BODY_INITIATING += "  cubeard) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_CUBE_ROOT --ard\"\n"
+BODY_INITIATING += "  cubeard) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_CUBE_ROOT --ard\"\n"
 BODY_INITIATING += "    ;;\n"
-BODY_INITIATING += "  none) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_SPHERE_ROOT\"\n"
+BODY_INITIATING += "  none) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_SPHERE_ROOT\"\n"
 BODY_INITIATING += "    ;;\n"
-BODY_INITIATING += "  boundary) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_SPHERE_ROOT --boundary\"\n"
+BODY_INITIATING += "  boundary) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_SPHERE_ROOT --boundary\"\n"
 BODY_INITIATING += "    ;;\n"
-BODY_INITIATING += "  warping) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_SPHERE_ROOT --warping\"\n"
+BODY_INITIATING += "  warping) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_SPHERE_ROOT --warping\"\n"
 BODY_INITIATING += "    ;;\n"
-BODY_INITIATING += "  warpingboundary) INIT_COMMAND_PREFIX = \"$INIT_COMMAND_SPHERE_ROOT --warping --boundary\"\n"
+BODY_INITIATING += "  warpingboundary) INIT_COMMAND_PREFIX=\"$INIT_COMMAND_SPHERE_ROOT --warping --boundary\"\n"
 BODY_INITIATING += "    ;;\n"
 BODY_INITIATING += "esac\n"
 BODY_INITIATING += "\n"
@@ -43,26 +43,44 @@ BODY_CONTINUING += "done\n"
 
 def generate_initiating_script(algorithm, func_name, dim, n_eval):
 	script = HEAD
-	script += "ALGORITHM=" + algorithm + "\n"
-	script += "TASK=" + func_name + "\n"
+	script += "ALGORITHM=\"" + algorithm + "\"\n"
+	script += "TASK=\"" + func_name + "\"\n"
 	script += "DIM=" + str(dim) + "\n"
 	script += "NEVAL=" + str(n_eval) + "\n"
 	script += "\n"
 	script += BODY_INITIATING
 	script += BODY_CONTINUING
-	print(script)
+
+	filename = '_'.join(["torque_run", algorithm, func_name, 'D' + str(dim), 'E' + str(n_eval) + '.sh'])
+	sh_file = open(filename, 'wt')
+	sh_file.write(script)
+	sh_file.close()
 
 
-def generate_continuing_script(pathname):
+def generate_continuing_script(pathname, n_eval):
 	script = HEAD
-	script += "EXPPATH=" + os.path.realpath(pathname) + "\n"
+	script += "EXPPATH=\"" + os.path.realpath(pathname) + "\"\n"
+	script += "NEVAL=" + str(n_eval) + "\n"
 	script += "\n"
 	script += BODY_CONTINUING
-	print(script)
+
+	filename = '_'.join(["torque_run", pathname, 'E' + str(n_eval) + '.sh'])
+	sh_file = open(filename, 'wt')
+	sh_file.write(script)
+	sh_file.close()
 
 
 if __name__ == '__main__':
 	if len(sys.argv) == 5:
 		generate_initiating_script(*sys.argv[1:])
-	elif len(sys.argv) == 2:
-		generate_continuing_script(sys.argv[1])
+	elif len(sys.argv) == 3:
+		generate_continuing_script(*sys.argv[1:])
+	else:
+		print("Argument should be")
+		print("1 : algorithm(none, boudanry, warping, warpingboundary, cube, cubeard")
+		print("2 : task(branin, hartmann6, rosenbrock, levy, styblinskitang, schwefel, michalewicz, camelback, qing, bird")
+		print("3 : dim(20, 50, 100)")
+		print("4 : n_eval")
+		print("OR")
+		print("1 : path")
+		print("2 : n_eval")
